a8b3631989a9085dff4148dd920fb302dd0a77f3
devmagda <113769198+devmagda@users.noreply.github.com>
2025-03-24 17:27:06 +0100

    MVC
    Basic MVC implementation.
    Controller controls model and view
    View currently only logs the entities position (placeholder for debug)
    Model only updates the position by incrementing x and y

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {Vector2D, VectorUtil} from "./modules/math/vectors/Vectors";
-import {Controller} from "./modules/mvc/MVC";
+import Controller from "./modules/mvc/Controller";
 
 const a = new Vector2D(1, 2);
 
