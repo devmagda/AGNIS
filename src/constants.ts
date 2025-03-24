31fecb66f4975d7a261658e6c3dbf3acf5022c7d
devmagda <113769198+devmagda@users.noreply.github.com>
2025-03-24 21:29:18 +0100

    Create constants.ts
    Constants currently only contain html ids for simpler referencing

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+const ButtonIds = {
+    ControllerPlay: "controller_play",
+    ControllerPause: "controller_pause",
+    ControllerNext: "controller_next",
+    ControllerReload: "controller_reload",
+};
+
+const CanvasId = "view";
+
+const DivIds = {
+    Game: "game",
+};
+
+export { ButtonIds, CanvasId, DivIds };
